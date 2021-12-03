15591e75a9f01b0f2c140753fcff39a058200bbe
Juan Ramirez <jua.ramirezp@gmail.com>
2021-12-02 21:50:17 -0300

    Upload solution

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 var app = new Vue({
 	el: "#app",
 	data: {
-		message: "Hola Vue!",
+		users: [],
+		searchInput: "",
+		loading: false,
+	},
+	mounted() {
+		this.loading = true;
+		fetch("https://randomuser.me/api/?results=50")
+			.then((response) => response.json())
+			.then((json) => {
+				this.users = json.results;
+				this.loading = false;
+			});
+	},
+	computed: {
+		filteredData: function () {
+			this.loading = true;
+			var data = [];
+
+			this.users.forEach((user) => {
+				if (
+					user.name.first
+						.toLowerCase()
+						.includes(this.searchInput.toLowerCase()) ||
+					user.name.last.toLowerCase().includes(this.searchInput.toLowerCase())
+				) {
+					data.push(user);
+				}
+			});
+
+			this.loading = false;
+			return data;
+		},
 	},
 });
